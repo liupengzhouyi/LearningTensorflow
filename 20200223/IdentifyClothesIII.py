5c4ce9e3d8b134ab9b16af26750829b09dc4644f
liupeng <liupeng.0@outlook.com>
2020-02-23 22:21:27 +0800

    衣服识别III V 1.0.4

--- 20200223/IdentifyClothesIII.py
+++ 20200223/IdentifyClothesIII.py
@@ -1,4 +1,8 @@
+import numpy
 import tensorflow
+import matplotlib.pyplot as plt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # 获取数据源
 fashion_mnist = tensorflow.keras.datasets.fashion_mnist
@@ -10,6 +14,8 @@ class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 train_images = train_images / 255
 test_images = test_images / 255
+
+print(test_labels[0])
 
 model = tensorflow.keras.Sequential()
 
@@ -25,9 +31,36 @@ model.compile(
 
 model.summary()
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=1)
+
+property = model.predict(test_images)
+
+plt.figure(figsize=(3, 9))
+
+def pp(i, property):
+    plt.subplot(5,2,2*i)
+    plt.xticks(range(10))
+    plt.bar(range(10), property, color='red')
+    plt.ylim([0, 1])
+    plt.xlabel('image01')
+
+def paly(i, index):
+    plt.subplot(5, 2, (i-1)*2 + 1)
+    propertys = property[index]
+    number = numpy.argmax(propertys)
+    class_name = class_names[number]
+    plt.imshow(test_images[index])
+    plt.xlabel(class_name)
+    plt.grid(False)
+    pp(i, propertys)
+
+
+paly(1, 101)
+paly(2, 108)
+paly(3, 102)
+paly(4, 151)
+paly(5, 191)
 
 
 
-
-
+plt.show()
